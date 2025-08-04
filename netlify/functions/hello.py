67470a814b73208c61a395fb4c92ef050f0c53bf
rocavence <rocavence@rengised.com>
2025-08-05 00:40:41 +0800

    改善 Python Functions 結構

--- netlify/functions/hello.py
+++ netlify/functions/hello.py
@@ -1,5 +1,12 @@
+# -*- coding: utf-8 -*-
+import json
+
 def handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Hello World from Python!'
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+        },
+        'body': json.dumps({'message': 'Hello from Python!'})
     }
