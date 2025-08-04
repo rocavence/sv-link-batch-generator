2e101daf895844fc3c43105270cb947fc2e2e0c7
rocavence <rocavence@rengised.com>
2025-08-05 00:47:33 +0800

    建立最標準的 Python Function

--- netlify/functions/hello.py
+++ netlify/functions/hello.py
@@ -1,12 +1,5 @@
-# -*- coding: utf-8 -*-
-import json
-
 def handler(event, context):
     return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'application/json'
-        },
-        'body': json.dumps({'message': 'Hello from Python!'})
+        "statusCode": 200,
+        "body": "Hello World"
     }
